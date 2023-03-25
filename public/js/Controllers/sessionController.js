0f8a246d226cf9ea67ed7fd68d6fa37bc26599e8
Erlan Rangel <erlanrangel@gmail.com>
2023-03-24 21:20:12 -0600

    Funcionalidad de login lista

---
+++ public/js/Controllers/sessionController.js
@@ -0,0 +1,26 @@
+"use strict";
+IdentifyLoggedUser();
+
+// @@@ NOT WORKING
+function IdentifyLoggedUser() {
+  let result = GetActiveSession();
+
+  if (result != null) {
+    let message = document.getElementById("welcome-message");
+    let rol = document.getElementById("rol");
+    message.innerHTML =
+      "<h1>Bievenid@ </h1>" +
+      "<p>" +
+      result.Nombre +
+      " " +
+      result.Apellido1 +
+      "</p>";
+    rol.innerHTML = "ROL: " + result.Rol;
+  }
+}
+// @@@
+
+function Logout() {
+  ClearActiveSession();
+  location.href = "login.html";
+}
---
+++ public/js/utils.js
@@ -0,0 +1,85 @@
+"use strict";
+
+function PrintError(msj) {
+  Swal.fire({
+    title: "Error!",
+    text: msj,
+    icon: "error",
+    confirmButtonText: "Return",
+  });
+}
+
+function PrintSuccess(msj) {
+  Swal.fire({
+    title: "Excelente!",
+    text: msj,
+    icon: "success",
+    confirmButtonText: "Ok",
+  });
+}
+
+/**
+ * Validacion de correo electronico con expresiones regulares
+ * El correo no debe contener espacios
+ *
+ * @param email
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+ * @see https://regex101.com/
+ */
+function ValidateEmail(pEmail) {
+  const email = pEmail.value;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailRegex.test(email)) {
+    PrintError("Ingrese un correo electronico valido");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+/**
+ * Valida las contraseñas
+ * Condiciones de contraseña:
+ * - Minimo 8 caracteres
+ * - Al menos una minuscula
+ * - Al menos una mayuscula,
+ * - Al menos un numero
+ * - La contraseña de confirmacion debe ser igual a la contraseña inicial
+ */
+function ValidatePasswords(pPass) {
+  const pass = pPass.value;
+
+  if (!pass || pass == "") {
+    PrintError("Contraseña es requerida");
+    return false;
+  }
+
+  if (pass.length < 8) {
+    PrintError("La contraseña es menor a 8 caracteres");
+    return false;
+  }
+
+  if (!/[a-z]/.test(pass)) {
+    PrintError("La contraseña debe tener al menos una minuscula");
+    return false;
+  }
+
+  if (!/[A-Z]/.test(pass)) {
+    PrintError("La contraseña debe tener al menos una mayuscula");
+    return false;
+  }
+
+  if (!/[\d]/.test(pass)) {
+    PrintError("La contraseña debe contener al menos un numero");
+    return false;
+  }
+
+  // if (passConf != pass) {
+  //   PrintError("Las contraseñas no coinciden");
+  //   return false;
+  // }
+
+  return true;
+}
