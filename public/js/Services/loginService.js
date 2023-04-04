3cd84052677a7a345ec6ef41252098cf50bc92aa
Erlan Rangel <erlanrangel@gmail.com>
2023-04-04 12:51:32 -0600

    [fix] Base de datos de prueba se accede de manera global

--- public/js/Services/loginService.js
+++ public/js/Services/loginService.js
@@ -1,30 +1,5 @@
 "use strict";
 
-let userTest = [
-  {
-    Nombre: "Erlan",
-    Apellido1: "Rangel",
-    Apellido2: "Garro",
-    User: "rangeler",
-    Pass: "asdfzxcv",
-    Rol: "Admin",
-    Email: "erangelg@ucenfotec.ac.cr",
-    Estatus: 1,
-    SuperUser: true,
-  },
-  {
-    Nombre: "Limberth",
-    Apellido1: "Vazquez",
-    Apellido2: "",
-    User: "lvasquez",
-    Pass: "qwerasdf",
-    Rol: "Provee",
-    Email: "lvasquez@xyz.com",
-    Estatus: 1,
-    SuperUser: false,
-  },
-];
-
 function AuthenticateUser(pUser, pPass) {
   let result = null;
 
---
+++ public/js/testDB.js
@@ -0,0 +1,24 @@
+let userTest = [
+  {
+    Nombre: "Erlan",
+    Apellido1: "Rangel",
+    Apellido2: "Garro",
+    User: "rangeler",
+    Pass: "asdfzxcv",
+    Rol: "Admin",
+    Email: "erangelg@ucenfotec.ac.cr",
+    Estatus: 1,
+    SuperUser: true,
+  },
+  {
+    Nombre: "Limberth",
+    Apellido1: "Vazquez",
+    Apellido2: "",
+    User: "lvasquez",
+    Pass: "qwerasdf",
+    Rol: "Provee",
+    Email: "lvasquez@xyz.com",
+    Estatus: 1,
+    SuperUser: false,
+  },
+];
